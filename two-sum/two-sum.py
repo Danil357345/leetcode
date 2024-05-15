1957790ce81825f60a8b51ba30dc6af87c167136
Danil357345 <128617132+Danil357345@users.noreply.github.com>
2024-05-15 18:28:00 +0300

    Update two-sum.py

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,1 +1,11 @@
 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_indices = {}  # Dictionary to store indices of numbers
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_indices:
+                return [num_indices[complement], i]
+            num_indices[num] = i
